3277f87ff15644d7ab221e5ad9118dad834ee995
noamit <noa1amit2@gmail.com>
2025-01-03 15:38:21 +0200

    update user details - get user from cookie

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const registerUser = require("../controller/registerUser");
+const checkEmail = require("../controller/checkEmail");
+const checkPassword = require("../controller/checkPassword");
+const userDetails = require("../controller/userDetails");
+const logout = require("../controller/logout");
+const updateUserDetails = require("../controller/updateUserDetails");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-
+router.post("/email", checkEmail);
+router.post("/password", checkPassword);
+router.get("/user-details", userDetails);
+router.post("/logout", logout);
+router.post("/update-user", updateUserDetails);
 module.exports = router;
