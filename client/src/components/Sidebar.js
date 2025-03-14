d3ec6caa168091868e2adb06a324426b72c0062c
noamit <noa1amit2@gmail.com>
2025-03-14 17:45:15 +0200

    EditUserDetails component on sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -5,9 +5,12 @@ import { FaUserPlus } from "react-icons/fa";
 import { BiLogOut } from "react-icons/bi";
 import { useSelector } from "react-redux";
 import Avatar from "./Avatar";
+import EditUserDetails from "./EditUserDetails";
 
 const Sidebar = () => {
   const user = useSelector((state) => state?.user);
+  const [editUserOpen, setEditUserOpen] = useState(true);
+
   return (
     <div className="w-full h-full grid grid-cols-[48px,1fr] bg-white">
       <div className="bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5 text-slate-600 flex flex-col justify-between">
@@ -29,18 +32,35 @@ const Sidebar = () => {
             <FaUserPlus size={20} />
           </div>
         </div>
-      </div>
 
-      <div className="flex flex-col items-center">
-        <button
-          title="logout"
-          className="w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded"
-        >
-          <span className="-ml-2">
-            <BiLogOut size={20} />
-          </span>
-        </button>
+        <div className="flex flex-col items-center">
+          <button
+            className="mx-auto"
+            title={user?.name}
+            onClick={() => setEditUserOpen(true)}
+          >
+            <Avatar
+              width={40}
+              height={40}
+              name={user?.name}
+              imageUrl={user?.profile_pic}
+              userId={user?._id}
+            />
+          </button>
+          <button
+            title="logout"
+            className="w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded"
+          >
+            <span className="-ml-2">
+              <BiLogOut size={20} />
+            </span>
+          </button>
+        </div>
       </div>
+      {/*edit user details*/}
+      {editUserOpen && (
+        <EditUserDetails onClose={() => setEditUserOpen(false)} user={user} />
+      )}
     </div>
   );
 };
